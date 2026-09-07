617edf1527408b98012ee35371b45a8876826489
Zakhar <lephiziel@gmail.com>
2026-09-07 13:28:44 +0500

    database connnection and repository

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,1 +1,26 @@
 package main
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"url-shortener/internal/repository"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+func main() {
+	// Database connection
+	pool, err := pgxpool.New(context.Background(), os.Getenv("DATABASE_URL"))
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Unable to connect to database: %v\n", err)
+		os.Exit(1)
+	}
+	defer pool.Close()
+
+	// Repository
+	repo := repository.NewRepository(pool)
+
+	// Service
+	// Service gets repo and then handler gets service
+}
--- internal/repository/repository.go
+++ internal/repository/repository.go
@@ -2,7 +2,11 @@ package repository
 
 import (
 	"context"
+	"errors"
 	"url-shortener/internal/domain"
+
+	"github.com/jackc/pgx/v5"
+	"github.com/jackc/pgx/v5/pgxpool"
 )
 
 type Repository interface {
@@ -12,21 +16,47 @@ type Repository interface {
 }
 
 type PostgresRepository struct {
-	// database
+	db *pgxpool.Pool
 }
 
-func NewRepository() *PostgresRepository { // Тут будем принимать бдшку
-	return &PostgresRepository{} // Сюда будем закидывать бдшку
+func NewRepository(db *pgxpool.Pool) *PostgresRepository {
+	return &PostgresRepository{
+		db: db,
+	}
 }
 
 func (r *PostgresRepository) NextID(ctx context.Context) (int64, error) {
-	return 0, nil
+	var id int64
+	err := r.db.QueryRow(ctx, "SELECT nextval('link_id_seq')").Scan(&id)
+	if err != nil {
+		return 0, err
+	}
+	return id, nil
 }
 
 func (r *PostgresRepository) SaveLink(ctx context.Context, link domain.Link) (domain.Link, error) {
-	return domain.Link{}, nil
+	_, err := r.db.Exec(ctx,
+		"INSERT INTO links (code, url, created_at) VALUES ($1, $2, $3)",
+		link.Code, link.URL, link.CreatedAt)
+
+	if err != nil {
+		return domain.Link{}, err
+	}
+	return link, nil
 }
 
-func (r *PostgresRepository) FindLink(code string) (domain.Link, error) {
-	return domain.Link{}, nil
+func (r *PostgresRepository) FindLink(ctx context.Context, code string) (domain.Link, error) {
+	var link domain.Link
+	err := r.db.QueryRow(ctx,
+		"SELECT code, url, created_at FROM links WHERE code = $1", code,
+	).Scan(&link.Code, &link.URL, &link.CreatedAt)
+
+	if errors.Is(err, pgx.ErrNoRows) {
+		return domain.Link{}, domain.ErrNotFound
+	}
+
+	if err != nil {
+		return domain.Link{}, err
+	}
+	return link, nil
 }
